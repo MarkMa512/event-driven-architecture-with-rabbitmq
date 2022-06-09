0dcf4b6168e3dd3fd44d1bddb4b64eb2bc84aa7b
MarkMa512 <ningzhi04@hotmail.com>
2022-06-09 15:28:27 +0800

    setup rabbitmq and containerised

---
+++ admin/products/producer.py
@@ -0,0 +1,22 @@
+import pika
+from os import environ
+
+hostname = environ.get('RABBIT_HOST') or 'rabbitmq'
+port = environ.get('RABBIT_PORT') or 5672
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(
+        host=hostname, port=port,
+        heartbeat=3600, blocked_connection_timeout=3600
+    )
+)
+
+channel = connection.channel()
+
+
+def publish():
+    channel.basic_publish(exchange='', routing_key='admin', body='hello')
+
+
+
+
--- main/main.py
+++ main/main.py
@@ -4,20 +4,35 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import UniqueConstraint
 import requests
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:root@db/main'
 CORS(app)
-
 db = SQLAlchemy(app)
+
+# https://www.youtube.com/watch?v=uNmWxvvyBGU
+migrate = Migrate(app, db)
 
 
 class Product(db.Model):
-    pass
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    title = db.Column(db.String(200))
+    image = db.Column(db.String(200))
+
+
+class ProductUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    product_id = db.Column(db.Integer)
+
+    UniqueConstraint('user_id', 'product_id', name='user_product_unique')
+
 
 @app.route('/')
 def index():
     return "Hello! Apple"
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
